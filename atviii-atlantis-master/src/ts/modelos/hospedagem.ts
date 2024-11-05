09941e44524272d8322e256e6380dc63ff37f2b8
Pedro Martins de Oliveira <142221456+OliveiraPedro09@users.noreply.github.com>
2024-11-05 17:38:40 -0300

    feat: Novo modelo Implementado
    Modelo de Hospedagem criado

---
+++ atviii-atlantis-master/src/ts/modelos/hospedagem.ts
@@ -0,0 +1,45 @@
+import { NomeAcomadacao } from "../enumeracoes/NomeAcomadacao";
+import Cliente from "./cliente";
+
+export default class Hospedagem {
+    
+    private acomodacao : NomeAcomadacao
+    private cliente : Cliente
+    private dataInicio : Date
+    private dataFim : Date
+
+    constructor(acomodacao : NomeAcomadacao, cliente : Cliente, dataInicio : Date, dataFim : Date){
+        this.acomodacao = acomodacao
+        this.cliente = cliente
+        this.dataInicio = dataInicio
+        this.dataFim = dataFim
+    }
+
+    public get Acomodacao(){
+        return this.acomodacao
+    }
+    public get Cliente(){
+        return this.cliente
+    }
+    public get DataInicio(){
+        return this.dataInicio
+    }   
+    public get DataFim(){
+        return this.dataFim
+    }
+
+
+    public setAcomodacao(acomodacao : NomeAcomadacao){
+        this.acomodacao = acomodacao
+    }
+    public setCliente(cliente : Cliente){
+        this.cliente = cliente
+    }
+    public setDataInicio(dataInicio : Date){
+        this.dataInicio = dataInicio
+    }
+    public setDataFim(dataFim : Date){
+        this.dataFim = dataFim
+    }
+
+}
